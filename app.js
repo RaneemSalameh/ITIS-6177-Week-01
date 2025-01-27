9ed8f360179794a76d25237dbb60169dfa22bd13
Raneem <salameh.ran@gmail.com>
2025-01-26 21:11:58 -0500

    Created Simple NodeJS Server

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const app = express();
+const http = require('http');
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
